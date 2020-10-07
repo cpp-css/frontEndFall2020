07e9a0b62ed7ee0c136d564e80c327996fb02ed5
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2020-10-07 01:48:17 -0700

    small fixes for #5
    refactor vaildation

--- screens/SignUp/SignUp.component.js
+++ screens/SignUp/SignUp.component.js
@@ -1,16 +1,54 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, Alert, Button} from 'react-native';
 
 // Components
-import Box from '../../components/Box/Box.component';
+import styles from './SignUp.styles';
 
-const SignUp = () => {
-  return (
-    <View>
-      <Text>I guess use this an exmaple on how to create a new component.</Text>
-      <Box />
-    </View>
-  );
+const SignUp = ({navigation}) => {
+
+    const [email, setEmail] = useState("");
+
+    const validateInput = () => {
+        const format = /^([\w\.\-]+)@cpp.edu/;
+        if (!format.test(email)) {
+            Alert.alert("Please input your student email.");
+        } else {
+            console.log("valid.");
+        }
+    };
+    /*
+    const email = () => {
+        const doSignUp = () => {
+            if (email === "") {
+                Alert.alert("Email Required");
+                setError("Email required *");
+                setValid(false);
+                return;
+            } else {
+                Alert.alert("Email is correct");
+            }
+        }
+    }
+    */
+
+    return (
+        <View>
+            <TextInput
+                styles={styles.textField}
+                placeholder="Email Address"
+                onChangeText={text => {
+                    setEmail(text);
+                }}
+                value={email}
+            />
+            <Button
+                styles={styles.signInButtonText}
+                title="Sign Up"
+                onPress={validateInput}
+            />
+        </View>
+    );
 };
+
 
 export default SignUp;
