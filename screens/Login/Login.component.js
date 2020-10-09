e61a735de39b6928f110bf159fcf7c371cac4af3
lchua2314 <lchua2314@gmail.com>
2020-10-08 21:12:31 -0700

    Changed indent to fou spaces each

--- screens/Login/Login.component.js
+++ screens/Login/Login.component.js
@@ -5,12 +5,12 @@ import {View, Text} from 'react-native';
 import Box from '../../components/Box/Box.component';
 
 const Login = () => {
-  return (
-    <View>
-      <Text>I guess use this an exmaple on how to create a new component.</Text>
-      <Box />
-    </View>
-  );
+    return (
+        <View>
+        <Text>I guess use this an exmaple on how to create a new component.</Text>
+        <Box />
+        </View>
+    );
 };
 
 export default Login;
