9fb979c54f02dc0a7483ab64447e661833d6bf8b
lchua2314 <lchua2314@gmail.com>
2020-10-17 18:47:58 -0700

    Fixed Icons not showing up; Changed from 2 spaces to 4 spaces in new files

--- screens/Main/tabbar/profile.js
+++ screens/Main/tabbar/profile.js
@@ -5,61 +5,61 @@ import * as eva from '@eva-design/eva';
 import {Avatar, Layout} from '@ui-kitten/components';
 
 class Profile extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <Layout style={styles.layout} level="1">
-          <React.Fragment>
-            <View style={styles.row}>
-              <Avatar
-                style={styles.avatar}
-                size="giant"
-                source={require('../../../assets/images/primary-logo.png')} // Change "Profile" image later (and rename CPP logo image)
-              />
-            </View>
-            <View style={styles.row}>
-              <Text style={styles.text} category="h2">
-                Ben Mai
-              </Text>
-            </View>
-            <View style={styles.row}>
-              <Button
-                onPress={() => this.props.navigation.navigate('contact')}
-                status="basic">
-                Add more interests
-              </Button>
-            </View>
-            <View style={styles.row}>
-              <Button
-                onPress={() => this.props.navigation.navigate('contact')}
-                status="basic">
-                Notification Settings
-              </Button>
-            </View>
-          </React.Fragment>
-        </Layout>
-      </ApplicationProvider>
-    );
-  }
+    render() {
+        return (
+            <ApplicationProvider {...eva} theme={eva.light}>
+                <Layout style={styles.layout} level="1">
+                    <React.Fragment>
+                        <View style={styles.row}>
+                            <Avatar
+                                style={styles.avatar}
+                                size="giant"
+                                source={require('../../../assets/images/primary-logo.png')} // Change "Profile" image later (and rename CPP logo image)
+                            />
+                        </View>
+                        <View style={styles.row}>
+                            <Text style={styles.text} category="h2">
+                                Ben Mai
+                            </Text>
+                        </View>
+                        <View style={styles.row}>
+                            <Button
+                                onPress={() => this.props.navigation.navigate('contact')}
+                                status="basic">
+                                Add more interests
+                            </Button>
+                        </View>
+                        <View style={styles.row}>
+                            <Button
+                                onPress={() => this.props.navigation.navigate('contact')}
+                                status="basic">
+                                Notification Settings
+                            </Button>
+                        </View>
+                    </React.Fragment>
+                </Layout>
+            </ApplicationProvider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  layout: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00FA9A',
-  },
-  avatar: {
-    width: 200,
-  },
-  row: {
-    marginTop: 20,
-  },
+    layout: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#00FA9A',
+    },
+    avatar: {
+        width: 200,
+    },
+    row: {
+        marginTop: 20,
+    },
 });
 
 export default Profile;
