3e74d8ca72bf60a59f311acd3490af2b78444d4c
Peter <peterc3989@gmail.com>
2020-10-18 01:17:41 -0700

    revert changes

--- screens/Login/Login.component.js
+++ screens/Login/Login.component.js
@@ -13,38 +13,38 @@ const axios = require('axios');
 const email = '';
 const password = '';
 const login = async () => {
-  const url = 'https://jsonplaceholder.typicode.com/posts';
+	const url = 'https://jsonplaceholder.typicode.com/posts';
 
-  const settings = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
+	const settings = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
 
-  const body = JSON.stringify({email, password});
+	const body = JSON.stringify({email, password});
 
-  try {
-    let response = await axios.get(url,settings,body);
+	try {
+		let response = await axios.get(url, settings, body);
 
-    // use response to authenticate
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
+		// use response to authenticate
+		console.log(response);
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 const Login = () => {
-  return (
-    <View>
-      <Btn
-        style={styles.Btn}
-        onPress={login}
-        title="Login"
-        accessibilityLabel="Login">
-        <Text style={styles.btnTxt}>Login</Text>
-      </Btn>
-    </View>
-  );
+	return (
+		<View>
+			<Btn
+				style={styles.Btn}
+				onPress={login}
+				title="Login"
+				accessibilityLabel="Login">
+				<Text style={styles.btnTxt}>Login</Text>
+			</Btn>
+		</View>
+	);
 };
 
 export default Login;
