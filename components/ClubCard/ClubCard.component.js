1e973eb3a23ea84c0818921f88acf4b0e74e274e
lchua2314 <lchua2314@gmail.com>
2020-11-01 00:45:23 -0700

    Added club cards

---
+++ components/ClubCard/ClubCard.component.js
@@ -0,0 +1,56 @@
+import React, { useState, useContext } from 'react';
+import { Text, Image, View, Alert, Modal } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Context } from '../../components/Context.js';
+import styles from './ClubCard.styles';
+
+const ClubCard = (props) => {
+
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [context, setContext] = useContext(Context);
+
+    return (
+        <View>
+            <TouchableOpacity
+                style={styles.container}
+                onPress={() => {
+                    setModalVisible(!isModalVisible)
+                }}>
+                <Text style={styles.org}> {props.org} </Text>
+                <Text> Related to: {props.relatedTo} </Text>
+            </TouchableOpacity>
+            <Modal animationType="slide"
+                transparent={true}
+                visible={isModalVisible}>
+                <View style={styles.containerPopUp}>
+                    <Text style={styles.orgPopUp}> {props.org} </Text>
+                    <Image style={styles.imagePopUp} source={props.source} />
+                    <Text style={styles.linkPopUp}> {props.link} </Text>
+                    <Text style={styles.infoPopUp}> {props.info} </Text>
+                    <Text style={styles.relatedToPopUp}> Related to: {props.relatedTo} </Text>
+                    <TouchableOpacity style={styles.btnPopUp}>
+                        <Text style={styles.btnText}
+                            onPress={
+                                () => {
+                                    Alert.alert("You successfully have subscribed to " 
+                                    + props.org + "!")
+                                    setContext([...context, props.title]);
+                                }
+                            }>Suscribe</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnPopUp}>
+                        <Text
+                            style={styles.btnText}
+                            onPress={() => {
+                                setModalVisible(!isModalVisible)
+                            }}>
+                            Exit
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </Modal>
+        </View>
+    )
+}
+
+export default ClubCard;
--- screens/Clubs/Clubs.component.js
+++ screens/Clubs/Clubs.component.js
@@ -1,10 +1,100 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, ScrollView, Dimensions } from 'react-native';
+// Components
+import { Searchbar } from 'react-native-paper';
+import ClubCard from '../../components/ClubCard/ClubCard.component';
+
+// Styles
+import styles from './Clubs.styles';
+
+const { width } = Dimensions.get('window');
+
+const cardItems = [
+    {
+        org: "Computer Science Society",
+        link: "https://github.com",
+        image: require("../../assets/images/CareerCenterWorkshop.jpg"),
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        relatedTo: "engineering, science, computer science"
+    },
+    {
+        org: "Software Engineering Association",
+        link: "https://github.com",
+        image: require("../../assets/images/CTF.png"),
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        relatedTo: "archery, math, engineering"
+    },
+    {
+        org: "IEEE",
+        link: "https://github.com",
+        image: require("../../assets/images/CareerCenterWorkshop.jpg"),
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        relatedTo: "science, hip-hop"
+    },
+    {
+        org: "Computer Science Society",
+        link: "https://github.com",
+        image: require("../../assets/images/CareerCenterWorkshop.jpg"),
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        relatedTo: "engineering, science, computer science"
+    },
+    {
+        org: "Software Engineering Association",
+        link: "https://github.com",
+        image: require("../../assets/images/CTF.png"),
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        relatedTo: "archery, math, engineering"
+    },
+    {
+        org: "IEEE",
+        link: "https://github.com",
+        image: require("../../assets/images/CareerCenterWorkshop.jpg"),
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        relatedTo: "science, hip-hop"
+    },
+]
 
 const Clubs = () => {
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const onChangeSearch = query => setSearchQuery(query);
+
+    let filteredCards = cardItems.filter(
+        (event) => {
+            return event.org.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1;
+        }
+    );
     return (
-        <View>
-            <Text> Clubs </Text>
+        <View style={{flex: 1}}>
+            <Searchbar
+                style={styles.searchBox}
+                placeholder="Search"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+            />
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                horizontal={false}
+                decelerationRate={0}
+                snapToInterval={width + 75}
+                snapToAlignment={"center"}
+                contentInset={{
+                    top: 0,
+                    left: 30,
+                    bottom: 0,
+                    right: 30
+                }}>
+                {filteredCards.map((card, id) =>
+                    <ClubCard
+                        key={id}
+                        title={card.title}
+                        org={card.org}
+                        link={card.link}
+                        source={card.image}
+                        info={card.info}
+                        relatedTo={card.relatedTo}
+                    />
+                )}
+            </ScrollView>
         </View>
     );
 };
