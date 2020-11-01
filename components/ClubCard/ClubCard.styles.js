2cd2983597eb0c59d9505dc8f987084afea625b2
lchua2314 <lchua2314@gmail.com>
2020-11-01 00:45:23 -0700

    Added club cards

---
+++ components/ClubCard/ClubCard.styles.js
@@ -0,0 +1,89 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+const { width } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 10,
+        //flex: 1,
+        //flexDirection: 'row',
+        //justifyContent: 'space-between',
+        backgroundColor: 'white',
+        width: width - 20,
+        margin: 10,
+        //height: height - 210,
+        //alignItems: 'center',
+        borderRadius: 10,
+        padding: "5%",
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 1,
+            height: 1
+        },
+        shadowOpacity: 0.24,
+        shadowRadius: 5,
+        elevation: 3
+    },
+    org: {
+        fontWeight: 'bold',
+        fontSize: 25,
+    },
+
+    containerPopUp: {
+        marginTop: 10,
+        //flex: 1,
+        //flexDirection: 'row',
+        //justifyContent: 'space-between',
+        backgroundColor: 'white',
+        // width: width - 40,
+        margin: 10,
+        // height: height - 105,
+        alignItems: 'center',
+        borderRadius: 10,
+        padding: "5%",
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 1,
+            height: 1
+        },
+        shadowOpacity: 0.24,
+        shadowRadius: 5,
+        elevation: 3
+    },
+    orgPopUp: {
+        fontWeight: 'bold',
+        fontSize: 25,
+        textAlign: "center",
+    },
+    imagePopUp: {
+        width: 200,
+        height: 200,
+        margin: "5%",
+        borderRadius: 10,
+    },
+    linkPopUp: {
+        textDecorationLine: 'underline',
+    },
+    infoPopUp: {
+        margin: 5,
+    },
+    relatedToPopUp: {
+        fontWeight: 'bold',
+    },
+
+    btnPopUp: {
+        backgroundColor: '#92d050',
+        padding: 5,
+        margin: 5,
+        width: 100,
+        borderRadius: 10,
+    },
+    btnText: {
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontWeight: 'bold'
+    }
+});
+
+export default styles;
