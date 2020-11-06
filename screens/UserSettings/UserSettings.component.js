9846cf6496b10d2e861d352d9c064c42bd3fdd18
Deric Kwok <30312964+exelarios@users.noreply.github.com>
2020-11-05 16:04:36 -0800

    Forgot to add useContext

--- screens/UserSettings/UserSettings.component.js
+++ screens/UserSettings/UserSettings.component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 
 import TextLabel from '../../components/TextLabel/TextLabel.component';
